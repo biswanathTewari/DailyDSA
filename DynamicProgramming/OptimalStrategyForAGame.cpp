7c7014d778a0acb9f78ab3206b0e6d7e57967bf8
biZan7X <bizantechX@gmail.com>
2020-06-07 22:57:29 +0530

    first commit

--- DynamicProgramming/OptimalStrategyForAGame.cpp
+++ DynamicProgramming/OptimalStrategyForAGame.cpp
@@ -1,34 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int Optimal(int a[],int n){
+int dp[1000][1000];
 
-  int dp[n][n];
+int Optimal(int a[],int n,int i,int j){
+
+  if(dp[i][j] != -1)
+    return dp[i][j];
 
   //baseCase
-  for(int i=0;i<n;i++){
-    for(int j =0;j<n;j++){
+  if(i == j)//range is 1
+    return dp[i][j] = a[i];
 
-      if(i == j)//range is 1
-        dp[i][j] = a[i];
-
-      if(i+1 == j)//range is 2
-        dp[i][j] = max(a[i],a[j]);
-
-    }
-  }
+  if(i+1 == j)//range is 2
+    return dp[i][j] = max(a[i],a[j]);
 
 
   //choiceProgram
-  for(int i=0;i<n;i++){
-    for(int j=0;j<n;j++){
+  dp[i][j] = max((a[i]+min(Optimal(a,n,i+2,j),Optimal(a,n,i+1,j-1))) , (a[j]+min(Optimal(a,n,i,j-2),Optimal(a,n,i+1,j-1))));
 
-      dp[i][j] = max((a[i]+min(dp[i+2][j],dp[i+1][j-1])) , (a[j]+min(dp[i][j-2],dp[i+1][j-1])));
-
-    }
-  }
-
-  return dp[0][n-1];
+  return dp[i][j];
 
 }
 
@@ -46,7 +37,9 @@ int main(){
     for(int i=0;i<n;i++)
       cin>>a[i];
 
-    cout<<Optimal(a,n)<<endl;
+    memset(dp,-1,sizeof(dp));
+
+    cout<<Optimal(a,n,0,n-1)<<endl;
 
   }
 

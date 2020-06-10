754aa2ff036e8fcf69beb97ac45e474868a0e615
biZan7X <bizantechX@gmail.com>
2020-06-10 20:32:41 +0530

    first commit

--- Graph/ShortestSourcetoDestinationPath.cpp
+++ Graph/ShortestSourcetoDestinationPath.cpp
@@ -1,99 +1,90 @@
-//biZanXsolutions
+//Wednesday, June 10, 2020 7:44 PM
+//variation of dijkstras + BFS
+
 #include<bits/stdc++.h>
 using namespace std;
-#define N 200005
-#define MOD 1000000007
-#define ll long long int
-#define ld long double
-#define sl unsigned long long int
-#define str string
-#define vll vector<ll>
-#define pb push_back
-#define fr(i,n) for(ll (i) = 0; (i) <(n) ; (i)=(i+1))
-#define frn(i,n) for(ll (i) = 1; (i) <=(n) ; (i)=(i+1))
-#define frev(i,n) for(ll (i) = n-1; (i) >=(0) ; (i)--)
-#define fr1(j,a,b,c) for(ll (j) = a; (j) <b ; (j)=(j+c))
-#define bw(t) while(t--)
-#define fast ios::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL)
-typedef pair<ll, pair<ll, ll>> xecs;
 
-class Item {
-public :
-  int row;
-  int col;
-  int dis;
+//global variables
+int n,m;
+int x,y;
+int a[21][21];
+int des[21][21];
+int vis[21][21];
+int dx[] = {0,0,1,-1};
+int dy[] = {1,-1,0,0};
 
-  Item(int x,int y,int z)
-    : row(x), col(y), dis(z)
-  {}
-};
+bool isSafe(int nx , int ny){
+  if(nx >= 0 && nx < n && ny >= 0 && ny < m  && a[nx][ny] == 1)//if a valid vertex & not visited & passabel = 1;
+    return true;
+  return false;
+}
 
 
-int shortestPath(){
+void bfs(){
 
-  int n,m,ti,tj;
-  cin>>n>>m;
-
-  int a[n][m];
-
-  int vis[n][m];
-  fr(i,n){
-    fr(j,m){
-      cin>>a[i][j];
-      if(a[i][j] == 0)
-        vis[i][j] = 1;
-      else vis[i][j] = 0;
+  //visited matrix and source to destination matrix
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      vis[i][j] = 0;
+      des[i][j] = INT_MAX;
     }
   }
 
-  cin>>ti>>tj;
-  Item source(0,0,0);
+  queue<pair<int,int>> q;
 
-  queue<Item> q;
-  q.push(source);
-  vis[source.row][source.col] = 1;
+  //pushing the source + des of source to source = 0
+  if(isSafe(0,0))
+  {
+    q.push(make_pair(0,0));
+    vis[0][0] = 1;
+    des[0][0] = 0;
+  }
 
   while(!q.empty()){
-    Item temp = q.front();
+
+    pair<int,int> p = q.front(); //instead of the min , we get the front() of the queue
     q.pop();
 
-    if(temp.row == ti && temp.col == tj)
-      return temp.dis;
+    for(int i=0;i<4;i++){ //checking all the neighbours of the vertex
+      int nx = p.first + dx[i];
+      int ny = p.second + dy[i];
 
-    //moving up
-    if(temp.row - 1 >= 0 && vis[temp.row-1][temp.col] == 0){
-      q.push(Item(temp.row-1,temp.col,temp.dis+1));
-      vis[temp.row-1][temp.col] = 1;
+      if(isSafe(nx,ny) && vis[nx][ny] == 0 && des[nx][ny] > des[p.first][p.second] + 1){
+        des[nx][ny] = des[p.first][p.second] + 1;
+        q.push(make_pair(nx,ny));
+        vis[nx][ny] = 1;
+      }
     }
 
-    //moving Down
-    if(temp.row + 1 < n && vis[temp.row+1][temp.col] == 0){
-      q.push(Item(temp.row+1,temp.col,temp.dis+1));
-      vis[temp.row+1][temp.col] = 1;
-    }
-
-    //moving left
-    if(temp.col - 1 >= 0 && vis[temp.row][temp.col-1] == 0){
-      q.push(Item(temp.row,temp.col-1,temp.dis+1));
-      vis[temp.row][temp.col-1] = 1;
-    }
-
-    //moving right
-    if(temp.col + 1 < m && vis[temp.row][temp.col+1] == 0){
-      q.push(Item(temp.row,temp.col+1,temp.dis+1));
-      vis[temp.row][temp.col+1] = 1;
-    }
   }
 
-  return -1;
 }
 
 
 int main(){
+
   int t;
   cin>>t;
   while(t--){
-    cout<<shortestPath()<<endl;
+
+    cin>>n>>m;
+
+    for(int i=0;i<n;i++){
+      for(int j=0;j<m;j++){
+
+        cin>>a[i][j];
+
+      }
+    }
+
+    cin>>x>>y;
+
+    bfs();
+
+    if(des[x][y] == INT_MAX)
+      cout<<-1<<endl;
+    else cout<<des[x][y]<<endl;
+
   }
   return 0;
 }

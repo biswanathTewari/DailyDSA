d46ea0ee8f89e8dba6fdee8f7a76701d3c09f39c
biZan7X <bizantechX@gmail.com>
2020-06-11 20:32:00 +0530

    first commit

--- Graph/CircleOfStrings.cpp
+++ Graph/CircleOfStrings.cpp
@@ -1,58 +1,41 @@
-//biZanXsolutions
 #include<bits/stdc++.h>
 using namespace std;
-#define N 200005
+
 #define M 26
-#define MOD 1000000007
-#define ll long long int
-#define ld long double
-#define sl unsigned long long int
-#define str string
-#define vll vector<ll>
-#define pb push_back
-#define fr(i,n) for(ll (i) = 0; (i) <(n) ; (i)=(i+1))
-#define frn(i,n) for(ll (i) = 1; (i) <=(n) ; (i)=(i+1))
-#define frev(i,n) for(ll (i) = n-1; (i) >=(0) ; (i)--)
-#define fr1(j,a,b,c) for(ll (j) = a; (j) <b ; (j)=(j+c))
-#define bw(t) while(t--)
-#define fast ios::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL)
-typedef pair<ll, pair<ll, ll>> xecs;
 
-
-void DFS(vector<int> g[M],vector<int> &vis,int index){
+void DFS(vector<int> g[],vector<int> &vis,int index){
 
   vis[index] = 1;
 
-
-  for(int i=0;i<g[index].size();i++){
-      if(!vis[g[index][i]])
-        DFS(g,vis,g[index][i]);
-  }
+  for(auto i=g[index].begin();i!=g[index].end();i++)
+    if(!vis[*i])
+      DFS(g,vis,*i);
 
 }
 
 
-bool isConnected(vector<int> g[M],vector<int> mark,int index){
+int isStronglyConnected(vector<int> g[],vector<int> mark,int index){
 
   vector<int> vis(M,0);
 
   DFS(g,vis,index);
 
   for(int i=0;i<M;i++)
-    if(mark[i] != vis[i])
-      return false;
+    if(vis[i] != mark[i])
+      return 0;
 
-  return true;
+  return 1;
 }
 
 
-bool isCycle(string s[],int n){
+int isCycle(string s[],int n){
 
-  vector<int> g[M];
+  vector<int> g[M]; //adjacency list
 
-  vector<int> mark(M,0);
+  vector<int> mark(M,0); //presence of letters
 
-  vector<int> in(M,0),out(M,0);
+  vector<int> in(M,0); //counts inwards directions
+  vector<int> out(M,0); //counts outwards directions
 
   for(int i=0;i<n;i++){
 
@@ -62,26 +45,25 @@ bool isCycle(string s[],int n){
     mark[f] = 1;
     mark[l] = 1;
 
+    //f -> l
     in[l]++;
     out[f]++;
 
-    g[f].pb(l);
+    g[f].push_back(l);
 
   }
 
-
+  //no.of in = out for each char
   for(int i=0;i<M;i++)
     if(in[i] != out[i])
-        return false;
+      return 0;
 
-  return isConnected(g,mark,s[0].front() - 'a');
+  return isStronglyConnected(g,mark,s[0].front()-'a');
 
 }
 
 
-
 int main(){
-  fast;
   int t;
   cin>>t;
   while(t--){

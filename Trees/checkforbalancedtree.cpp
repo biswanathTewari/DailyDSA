737ee86632f32ce02984af5a1e912ca8896a6708
biZan7X <bizantechX@gmail.com>
2020-06-26 12:44:32 +0530

    first commit

---
+++ Trees/checkforbalancedtree.cpp
@@ -0,0 +1,134 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+// Function to Build Tree
+Node* buildTree(string str)
+{
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if(currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if(currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+bool isBalanced(Node *root);
+
+int main()
+{
+
+    int t;
+	scanf("%d ",&t);
+    while(t--)
+    {
+        string s;
+		getline(cin,s);
+        Node* root = buildTree(s);
+        cout << isBalanced(root) << endl;
+    }
+    return 1;
+}// } Driver Code Ends
+
+
+// This function should return tree if passed  tree
+// is balanced, else false.
+
+
+int bizan(Node *root,bool &ch){
+
+    //height if root is null
+    if(root == NULL) return 0;
+
+    //height if no child
+    if(!root->left && !root->right) return 1;
+
+    int ls=bizan(root->left,ch); //max height of the left recursive subtree
+    int rs=bizan(root->right,ch); //max height of the right recursive subtree
+
+    int diff=abs(ls-rs);
+
+    if(diff > 1 )
+          ch=false; //changing the value of the reference variable
+
+    return max(ls,rs)+1; //returning the max height of this recursive subtree
+}
+
+bool isBalanced(Node *root)
+{
+    if(root == NULL) return true;
+
+    bool ch=true;
+    bizan(root,ch);
+
+    return ch;
+}

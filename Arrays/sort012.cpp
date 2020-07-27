6377d9da26c7c71baf77d3e623c2770fd14865db
biZan7X <bizantechX@gmail.com>
2020-07-27 23:22:43 +0530

    first commit

--- Arrays/sort012.cpp
+++ Arrays/sort012.cpp
@@ -3,34 +3,23 @@ using namespace std;
 
 #define ll long long int
 
-void sorted(ll a[],ll n){ //time : O(N) : space : O(1)
+void sorted(ll a[],ll n){ //time : O(log N) : space : O(1)
 
-  ll counter[3] = {0}; //counts the number of 0s 1s and 2s
+  ll high = n-1;
+  ll low = 0;
+  ll mid = 0;
 
-  for(ll i=0;i<n;i++)
-    if(a[i] == 0)
-      counter[0]++;
-    else if(a[i] == 1)
-      counter[1]++;
-    else counter[2]++;
+  while(mid <= high){
 
+    if(a[mid] == 0 )
+      swap(a[mid++],a[low++]);
 
-  //printing the sorted array
-  for(ll i=0;i<n;i++)
-    if(counter[0]){
-      cout<<0<<" ";
-      counter[0]--;
-    }
-    else if(counter[1]){
-      cout<<1<<" ";
-      counter[1]--;
-    }
-    else if(counter[2]){
-      cout<<2<<" ";
-      counter[2]--;
-    }
+    else if(a[mid] == 1) //simply ignore
+      mid++;
+      
+    else swap(a[mid],a[high--]); //next check will start from current mid only, due to the swap from high
 
-  cout<<endl;
+  }
 
 }
 
@@ -50,6 +39,10 @@ int main(){
       cin>>a[i];
 
     sorted(a,n);
+
+    for(ll i=0;i<n;i++)
+      cout<<a[i]<<" ";
+    cout<<endl;
 
   }
 
